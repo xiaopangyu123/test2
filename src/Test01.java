105ce42b248c4a458a8fcb5856cb7250e16a06e3
cj <1194229898@qq.com>
2021-08-09 23:28:14 +0800

    Initial commit

---
+++ src/Test01.java
@@ -0,0 +1,8 @@
+public class Test01 {
+    public static void main(String[] args) {
+        Test01 t1= new Test01();
+        Test01 t2= new Test01();
+        System.out.println(t1.hashCode());
+        System.out.println(t2.hashCode());
+    }
+}
---
+++ src/Test02.java
@@ -0,0 +1,30 @@
+public class Test02 {
+    StringBuilder res;
+    int count = 0, n;
+    char[] num, loop = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    public  String printNumbers(int n) {
+        this.n = n;
+        res = new StringBuilder(); // 数字字符串集
+        num = new char[n]; // 定义长度为 n 的字符列表
+        dfs(0); // 开启全排列递归
+        res.deleteCharAt(res.length() - 1); // 删除最后多余的逗号
+        return res.toString(); // 转化为字符串并返回
+    }
+    public void dfs(int x) {
+        if(x == n) { // 终止条件：已固定完所有位
+            res.append(String.valueOf(num) + ","); // 拼接 num 并添加至 res 尾部，使用逗号隔开
+            return;
+        }
+        for(char i : loop) { // 遍历 ‘0‘ - ’9‘
+            num[x] = i; // 固定第 x 位为 i
+            dfs(x + 1); // 开启固定第 x + 1 位
+        }
+    }
+    public static void main(String[] args) {
+        Test02 t0 = new Test02();
+        String s =t0.printNumbers(2);
+        System.out.println(s);
+    }
+    //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
+
+}
